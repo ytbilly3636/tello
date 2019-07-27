ee063e15eba5d220ca4de28a0b80ce91324006b8
ytbilly3636 <tanaka@technosend.co.jp>
2019-07-27 16:07:04 +0900

    set yaw insstead of y, close #4

--- sample/follow.py
+++ sample/follow.py
@@ -17,11 +17,9 @@ class Follow(StateMachine):
         area_target = 30000, 
         area_th     = 5000, 
         center_th   = 100,
-        tr          = 1.0,
-        vr          = 0.25,
-        tg          = 0.25,
-        vx          = 0.1,
-        vy          = 0.1,
+        tg          = 0.2,
+        vx          = 0.2,
+        vr          = 0.4,
         vz          = 0.4,
         dbg         = False
         ):
@@ -34,11 +32,9 @@ class Follow(StateMachine):
         self._area_target = area_target
         self._area_th = area_th
         self._center_th = center_th
-        self._tr = tr
-        self._vr = vr
         self._tg = tg
         self._vx = vx
-        self._vy = vy
+        self._vr = vr
         self._vz = vz
 
         # debug mode
@@ -78,10 +74,6 @@ class Follow(StateMachine):
                 print('********** face is found **********')
                 return self._sm_follow_person
 
-            # face is not found
-            if not self._debug:
-                self._drone.controll(t=self._tr, vr=self._vr)
-
 
     def _sm_follow_person(self):
         while self.is_working():
@@ -111,18 +103,18 @@ class Follow(StateMachine):
 
             # controll to adjust
             adj_x = 0.0
-            adj_y = 0.0
+            adj_r = 0.0
             adj_z = 0.0
             if abs(area - self._area_target) > self._area_th:
                 adj_x = self._vx if area < self._area_target     else -1 * self._vx
             if abs(center[0] - center_target[0]) > self._center_th:
-                adj_y = self._vy if center[0] < center_target[0] else -1 * self._vy
+                adj_r = self._vr if center[0] > center_target[0] else -1 * self._vr
             if abs(center[1] - center_target[1]) > self._center_th:
                 adj_z = self._vz if center[1] < center_target[1] else -1 * self._vz
 
-            print(adj_x, adj_y, adj_z)
+            print(adj_x, adj_r, adj_z)
             if not self._debug:
-                self._drone.controll(t=self._tg, vx=adj_x, vy=adj_y, vz=adj_z)
+                self._drone.controll(t=self._tg, vx=adj_x, vr=adj_r, vz=adj_z)
 
 
 if __name__ == '__main__':
